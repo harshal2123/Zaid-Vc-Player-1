481207707171171ce65e5d904028525719338131
VENOMxCRAZY <87589227+VENOMxCRAZY@users.noreply.github.com>
2021-09-09 18:18:55 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -157,7 +157,7 @@ async def help_(client: Client, message: Message):
 @Client.on_message(command(["ping", f"ping@{BOT_USERNAME}"]) & ~filters.edited)
 async def ping_pong(client: Client, message: Message):
     start = time()
-    m_reply = await message.reply_text(V Pɪɴɴɢ...")
+    m_reply = await message.reply_text("V Pɪɴɴɢ...")
     delta_ping = time() - start
     await m_reply.edit_text(
         "V `ᴘᴏɴɢ!!`\n"
