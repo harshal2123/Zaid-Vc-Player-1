b392b3f102daefe11839497763d557a41d979f1d
VENOMxCRAZY <87589227+VENOMxCRAZY@users.noreply.github.com>
2021-09-10 12:16:47 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -64,7 +64,7 @@ async def start_(client: Client, message: Message):
                         "😁 ᴏꜰꜰɪᴄɪᴀʟ ᴢᴀɪᴅ ᴄʜᴀᴛ", url="https://t.me/global_girls_and_boys_chatting2")
                 ],[
                     InlineKeyboardButton(
-                        "😉 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ😍", url="https://pornhub.com "
+                        "😉 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ😍", url="https://github.com/VENOMxCRAZY/Zaid-Vc-Player "
                     )
                 ]
             ]
